ddf10b6b3672e172e76900b9b059688db22f61d6
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-03-19 23:26:38 +0000

    403 APi added

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def unauthorized():
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def forbidden():
     """GET /api/v1/forbidden
     Return:
       - 403 error
